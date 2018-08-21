06ce704c732851ec156fee9b8358c85ffa071c74
David Ellison <davidehellison@gmail.com>
2018-08-20 21:44:26 -0700

    some progress

--- src/DependencyTrees.jl
+++ src/DependencyTrees.jl
@@ -1,67 +1,78 @@
 module DependencyTrees
 
-export DependencyGraph
+export
+    DependencyGraph,
+    LabeledDependency,
+    TypedDependency,
+    UntypedDependency
 
-using AbstractTrees
 using LightGraphs
 
+# TODO: move interface into its own file?
 abstract type Dependency end
 
+_ni(f, dep) = error("$f not implemented for type $(typeof(t))!")
+
+# Dependency API:
+deprel(node::Dependency) = _ni(deprel, node)
+form(node::Dependency) = _ni(form, node)
+head(node::Dependency) = _ni(head, node)
+isroot(node::Dependency) = _ni(isroot, node)
+root(node::Dependency) = _ni(root, node)
+root(t::Type{Dependency}) = _ni(root, t)
+# end of Dependency API
+
 struct DependencyToken{T} <: Dependency
+    id::Int
     form::String
     data::T
     head::Int
 end
 
-DependencyToken(p::Pair) = DependencyToken(p.first, nothing, p.second)
-DependencyToken(t::Tuple{String,Int}) = DependencyToken(t[1], nothing, t[2])
-DependencyToken{T}(t::Tuple{String,T,Int}) where T = DependencyToken{T}(t...)
-DependencyToken(token, head) = DependencyToken(token, nothing, head)
-DependencyToken(token, arc, head) = DependencyToken(token, arc, head)
+deprel(d::DependencyToken) = d.data
+id(d::DependencyToken) = d.id
+form(d::DependencyToken) = d.form
+head(d::DependencyToken) = d.head
 
 const UntypedDependency = DependencyToken{Nothing}
+UntypedDependency(id, form, head) = DependencyToken(id, form, nothing, head)
+deprel(d::UntypedDependency) = nothing
 isroot(d::UntypedDependency) = (d.form == "ROOT")
-root(::Type{UntypedDependency}) = DependencyToken("ROOT", nothing, 0)
-root(d::UntypedDependency) = DependencyToken("ROOT", nothing, 0)
+root(::Type{UntypedDependency}) = DependencyToken(0, "ROOT", nothing, 0)
+root(d::UntypedDependency) = DependencyToken(0, "ROOT", nothing, 0)
 
 const LabeledDependency = DependencyToken{String}
 isroot(d::LabeledDependency) = (d.form == "ROOT" && d.data == "ROOT")
-root(::Type{LabeledDependency}) = DependencyToken("ROOT", "ROOT", 0)
-root(d::LabeledDependency) = DependencyToken("ROOT", "ROOT", 0)
+postag(d::LabeledDependency) = d.data
+deprel(d::LabeledDependency) = d.data
+root(::Type{LabeledDependency}) = DependencyToken(0, "ROOT", "ROOT", 0)
+root(d::LabeledDependency) = DependencyToken(0, "ROOT", "ROOT", 0)
 
+import Base.==
+==(d1::DependencyToken, d2::DependencyToken) =
+    d1.id == d2.id && d1.form == d2.form && d1.data == d2.data && d1.head == d2.head
 
 struct DependencyGraph{T<:DependencyToken} <: AbstractGraph{Int}
     graph::SimpleDiGraph
-    dependencies::Vector{T}
+    tokens::Vector{T}
 end
 
-DependencyGraph(sentence; add_root=true) =
-    DependencyGraph(DependencyToken{String}, sentence, add_root=add_root)
+function DependencyGraph(t::Type{<:Dependency}, tokens; add_root=true)
+    DependencyGraph([t(i, tk...) for (i,tk) in enumerate(tokens)], add_root=add_root)
+end
 
-function DependencyGraph(t::Type{<:Dependency}, sentence; add_root=true)
-    deps = [t(token) for token in sentence]
-    add_root && (deps = [root(t) ; deps])
-    graph = SimpleDiGraph(length(deps))
-    for (i, dep) in enumerate(deps)
-        add_edge!(graph, i, dep.head)
+function DependencyGraph(tokens::Vector{<:DependencyToken}; add_root=true)
+    add_root && (tokens = [root(eltype(tokens)) ; tokens])
+    graph = SimpleDiGraph(length(tokens))
+    for dep in tokens
+        isroot(dep) && continue
+        add_edge!(graph, id(dep), head(dep))
     end
-    DependencyGraph{t}(graph, deps)
+    return DependencyGraph(graph, tokens)
 end
 
-Base.length(g::DependencyGraph) = length(g.dependencies)
-
-# AbstractTrees.children(
-
-LightGraphs.edges(g::DependencyGraph) = edges(g.graph)
-# Base.eltype
-LightGraphs.has_edge(g::DependencyGraph, s, d) = has_edge(g.graph, s, d)
-LightGraphs.has_vertex(g::DependencyGraph, s, d) = has_vertex(g.graph, s, d)
-LightGraphs.inneighbors(g::DependencyGraph, v) = inneighbors(g.graph, v)
-LightGraphs.ne(g::DependencyGraph) = ne(g.graph)
-LightGraphs.nv(g::DependencyGraph) = nv(g.graph)
-LightGraphs.outneighbors(g::DependencyGraph, v) = outneighbors(g.graph, v)
-LightGraphs.vertices(g::DependencyGraph) = vertices(g.graph)
-LightGraphs.is_directed(::Type{DependencyGraph}) = true
-LightGraphs.is_directed(g::DependencyGraph) = true
+==(g1::DependencyGraph, g2::DependencyGraph) = all(g1.tokens .== g2.tokens)
+Base.getindex(g::DependencyGraph, i) = g.tokens[i]
+Base.length(g::DependencyGraph) = length(g.tokens)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,54 @@
 using DependencyTrees, Test
 
-# synthesis lectures book
-@testset "kubler et al 09" begin
+@testset "Tokens" begin
+
+    using DependencyTrees: deprel, form, id, head, root, isroot
+
+    @testset "Untyped Dependencies" begin
+        r = root(UntypedDependency)
+        @test isroot(r)
+        sent = [
+            ("The", 2),
+            ("cat", 3),
+            ("slept", 0),
+            (".", 3)
+        ]
+        for (i, token) in enumerate(sent)
+            dep = UntypedDependency(i, token...)
+            @test deprel(dep) == nothing
+            @test form(dep) == token[1]
+            @test id(dep) == i
+            @test !isroot(dep)
+            @test head(dep) == token[2]
+        end
+
+        g1 = DependencyGraph([UntypedDependency(i, t...) for (i, t) in enumerate(sent)])
+        g2 = DependencyGraph(UntypedDependency, sent)
+        @test g1 == g2
+    end
+
+    @testset "Labeled Dependencies" begin
+        r = root(LabeledDependency)
+        @test isroot(r)
+        sent = [
+            ("The", "DT", 2),
+            ("cat", "NN", 3),
+            ("slept", "VBD", 0),
+            (".", ".", 3)
+        ]
+        for (i, token) in enumerate(sent)
+            dep = LabeledDependency(i, token...)
+            @test form(dep) == token[1]
+            @test deprel(dep) == token[2]
+            @test head(dep) == token[3]
+            @test id(dep) == i
+            @test !isroot(dep)
+        end
+    end
+end
+
+@testset "Graphs" begin
+
     sent = [
         ("Economic", "ATT", 2),
         ("news", "SBJ", 3),
@@ -9,17 +56,19 @@ using DependencyTrees, Test
         ("little", "ATT", 5),
         ("effect", "OBJ", 3),
         ("on", "ATT", 5),
-        ("ﬁnancial", "ATT", 8),
+        ("financial", "ATT", 8),
         ("markets", "PC", 6),
         (".", "PU", 3),
     ]
-    graph_noroot = DependencyGraph(sent, add_root=false)
-    @test length(graph_noroot) == 9
-    graph = DependencyGraph(sent)
-    @test length(graph) == 10
-end
+    deps = [LabeledDependency(id, tok...) for (id, tok) in enumerate(sent)]
 
-@testset "wsj01" begin
+    graph = DependencyGraph(deps, add_root=true)
+    graph_noroot = DependencyGraph(deps, add_root=false)
+
+    @test length(graph) == 10
+    @test length(graph_noroot) == 9
+
+
     sent = [
         ("Pierre", "NNP", 2),
         ("Vinken", "NNP", 8),
@@ -40,6 +89,14 @@ end
         ("29", "CD", 16),
         (".", ".", 8)
     ]
-    graph_noroot = DependencyGraph(sent, add_root=false)
+    deps = [LabeledDependency(id, tok...) for (id, tok) in enumerate(sent)]
+
+    graph = DependencyGraph(deps, add_root=true)
+    @test DependencyTrees.isroot(graph[1])
+
+    graph_noroot = DependencyGraph(deps, add_root=false)
+    @test !DependencyTrees.isroot(graph_noroot[1])
+
+    @test length(graph) == 19
     @test length(graph_noroot) == 18
 end
