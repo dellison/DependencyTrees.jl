ca3b8d0d46723eaa08d4f48bc0f0da69c88727ee
David Ellison <davidehellison@gmail.com>
2018-08-24 22:44:37 -0700

    arc-eager parser configurations

--- src/parse/transition_parsers.jl
+++ src/parse/transition_parsers.jl
@@ -24,28 +24,27 @@ function ArcStandardConfig{T}(words) where T
     ArcStandardConfig{T}(stack, word_buffer, relations)
 end
 
-function leftarc(state::ArcStandardConfig, args...)
+function leftarc(state::ArcStandardConfig, args...; kwargs...)
     # assert a head-dependent relation between the word at the top of
     # the stack and the word directly beneath it; remove the lower
     # word from the stack
     @assert length(state.stack) >= 2
-    head = state.stack[end]
-    dependent = state.stack[end-1]
-    stack = [state.stack[1:end-2] ; [head]]
+    stack = state.stack
+    head, dependent = stack[end], stack[end-1]
+    stack = [stack[1:end-2] ; [head]]
     relations = state.relations
-    relations[dependent] = dep(relations[dependent], args...; head=head)
+    relations[dependent] = dep(relations[dependent], args...; head=head, kwargs...)
     ArcStandardConfig(stack, state.word_buffer, relations)
 end
 
-function rightarc(state::ArcStandardConfig, args...)
+function rightarc(state::ArcStandardConfig, args...; kwargs...)
     # assert a head-dependent relation btwn the 2nd word on the stack
     # and the word on top; remove the word at the top of the stack
     @assert length(state.stack) >= 2
-    dependent = state.stack[end]
-    head = state.stack[end-1]
+    dependent, head = state.stack[end], state.stack[end-1]
     stack = [state.stack[1:end-2] ; [head]]
     relations = state.relations
-    relations[dependent] = dep(relations[dependent], args...; head=head)
+    relations[dependent] = dep(relations[dependent], args...; head=head, kwargs...)
     ArcStandardConfig(stack, state.word_buffer, relations)
 end
 
@@ -61,11 +60,60 @@ end
 isfinal(state::ArcStandardConfig) =
     length(state.stack) == 1 && state.stack[1] == 0 && length(state.word_buffer) == 0
 
-# function parse(t::Type{<:Dependency}, words, oracle)
-#     state = ArcStandardConfig()
-#     while !isfinal(state)
-#         f = oracle(state)
-#         state = f(state)
-#     end
-#     return state
-# end
+
+"""
+    ArcEagerConfig{T<:Dependency}(words)
+
+Parser configuration for arc-eager transition-based dependency
+parsing.
+"""
+struct ArcEagerConfig{T} <: TransitionParserConfiguration{T}
+    stack::Vector{Int}
+    word_buffer::Vector{Int}
+    relations::Vector{T}
+end
+
+function ArcEagerConfig{T}(words) where T
+    stack = [0]
+    word_buffer = collect(1:length(words))
+    relations = [unk(T, id, w) for (id,w) in enumerate(words)]
+    ArcEagerConfig{T}(stack, word_buffer, relations)
+end
+
+function leftarc(state::ArcEagerConfig, args...; kwargs...)
+    # Assert a head-dependent relation between the word at the front
+    # of the input buffer and the word at the top of the stack; pop
+    # the stack.
+    head, dependent = state.word_buffer[1], state.stack[end]
+    relations = state.relations
+    if dependent > 0
+        relations[dependent] = dep(relations[dependent], head=head, args...; kwargs...)
+    end
+    ArcEagerConfig(state.stack[1:end-1], state.word_buffer, relations)
+end
+
+function rightarc(state::ArcEagerConfig, args...; kwargs...)
+    # Assert a head-dependent relation between the word on the top of
+    # the stack and the word at front of the input buffer; shift the
+    # word at the front of the input buffer to the stack.
+    head, dependent = state.stack[end], state.word_buffer[1]
+    relations = state.relations
+    relations[dependent] = dep(relations[dependent], head=head, args...; kwargs...)
+    buf = state.word_buffer
+    ArcEagerConfig([state.stack ; buf[1]], buf[2:end], relations)
+end
+
+function shift(state::ArcEagerConfig)
+    # Remove the word from the front of the input buffer and push it
+    # onto the stack.
+    buf = state.word_buffer
+    ArcEagerConfig([state.stack ; buf[1]], buf[2:end], state.relations)
+end
+
+function Base.reduce(state::ArcEagerConfig)
+    # Pop the stack.
+    ArcEagerConfig(state.stack[1:end-1], state.word_buffer, state.relations)
+end
+
+isfinal(state::ArcEagerConfig) =
+    length(state.stack) == 1 && state.stack[1] == 0 && length(state.word_buffer) == 0
--- test/test_transition_parsers.jl
+++ test/test_transition_parsers.jl
@@ -1,7 +1,8 @@
 using DependencyTrees, Test
-using DependencyTrees: ArcStandardConfig, leftarc, rightarc, shift, isfinal
 
 @testset "ArcStandard" begin
+
+    using DependencyTrees: ArcStandardConfig, leftarc, rightarc, shift, isfinal
 
     # figure 13.7 in jurafsky & martin SLP 3rd ed., aug 2018 draft
     sent = ["book", "me", "the", "morning", "flight"]
@@ -143,6 +144,157 @@ using DependencyTrees: ArcStandardConfig, leftarc, rightarc, shift, isfinal
         @test words(state.word_buffer) == []
         state = rightarc(state, "PRED")
         @test hasdeprel(state, "root", "PRED", "book") # (root -[pred]-> book)
+
+        # step 10
+        @test words(state.stack) == ["root"]
+        @test words(state.word_buffer) == []
+        @test isfinal(state)
+    end
+end
+
+@testset "ArcEager" begin
+
+    # figure 13.7 in jurafsky & martin SLP 3rd ed., aug 2018 draft
+    sent = ["book", "the", "flight", "through", "houston"]
+
+    word2id = Dict(word => id for (id, word) in enumerate(sent))
+    word2id["root"] = 0
+
+    words(ids) = [(id == 0 ? "root" : sent[id]) for id in ids]
+
+    @testset "Untyped" begin
+
+        using DependencyTrees: ArcEagerConfig, leftarc, rightarc, shift, reduce, isfinal
+
+        # head --> dep
+        hasdep(state, head, dep) = state.relations[word2id[dep]].head == word2id[head]
+
+        # step 0
+        state = ArcEagerConfig{UntypedDependency}(sent)
+        @test words(state.stack) == ["root"]
+        @test words(state.word_buffer) == ["book", "the", "flight", "through", "houston"]
+        state = rightarc(state)
+        @test hasdep(state, "root", "book") # (root --> book)
+
+        # step 1
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == ["the", "flight", "through", "houston"]
+        state = shift(state)
+
+        # step 2
+        @test words(state.stack) == ["root", "book", "the"]
+        @test words(state.word_buffer) == ["flight", "through", "houston"]
+        state = leftarc(state)
+        @test hasdep(state, "flight", "the") # (the <-- flight)
+
+        # step 3
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == ["flight", "through", "houston"]
+        state = rightarc(state)
+        @test hasdep(state, "book", "flight") # (book --> flight)
+
+        # step 4
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == ["through", "houston"]
+        state = shift(state)
+
+        # step 5
+        @test words(state.stack) == ["root", "book", "flight", "through"]
+        @test words(state.word_buffer) == ["houston"]
+        state = leftarc(state)
+        @test hasdep(state, "houston", "through") # (through <-- houston)
+
+        # step 6
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == ["houston"]
+        state = rightarc(state)
+        @test hasdep(state, "flight", "houston") # (flight --> houston)
+
+        # step 7
+        @test words(state.stack) == ["root", "book", "flight", "houston"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
+
+        # step 8
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
+
+        # step 9
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
+        @test hasdep(state, "root", "book") # (root --> book)
+
+        # step 10
+        @test words(state.stack) == ["root"]
+        @test words(state.word_buffer) == []
+        @test isfinal(state)
+    end
+
+    @testset "Typed" begin
+
+        using DependencyTrees: ArcEagerConfig, leftarc, rightarc, shift, reduce, isfinal
+
+        # head --> dep
+        hasdeprel(state, head, deprel, dep) =
+            state.relations[word2id[dep]].head == word2id[head] && state.relations[word2id[dep]].deprel == deprel
+
+        # step 0
+        state = ArcEagerConfig{TypedDependency}(sent)
+        @test words(state.stack) == ["root"]
+        @test words(state.word_buffer) == ["book", "the", "flight", "through", "houston"]
+        state = rightarc(state, "pred")
+        @test hasdeprel(state, "root", "pred", "book") # (root --> book)
+
+        # step 1
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == ["the", "flight", "through", "houston"]
+        state = shift(state)
+
+        # step 2
+        @test words(state.stack) == ["root", "book", "the"]
+        @test words(state.word_buffer) == ["flight", "through", "houston"]
+        state = leftarc(state, "det")
+        @test hasdeprel(state, "flight", "det", "the") # (the <-- flight)
+
+        # step 3
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == ["flight", "through", "houston"]
+        state = rightarc(state, "dobj")
+        @test hasdeprel(state, "book", "dobj", "flight") # (book --> flight)
+
+        # step 4
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == ["through", "houston"]
+        state = shift(state)
+
+        # step 5
+        @test words(state.stack) == ["root", "book", "flight", "through"]
+        @test words(state.word_buffer) == ["houston"]
+        state = leftarc(state, "pobj")
+        @test hasdeprel(state, "houston", "pobj", "through") # (through <-- houston)
+
+        # step 6
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == ["houston"]
+        state = rightarc(state, "mod")
+        @test hasdeprel(state, "flight", "mod", "houston") # (flight --> houston)
+
+        # step 7
+        @test words(state.stack) == ["root", "book", "flight", "houston"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
+
+        # step 8
+        @test words(state.stack) == ["root", "book", "flight"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
+
+        # step 9
+        @test words(state.stack) == ["root", "book"]
+        @test words(state.word_buffer) == []
+        state = reduce(state)
 
         # step 10
         @test words(state.stack) == ["root"]
