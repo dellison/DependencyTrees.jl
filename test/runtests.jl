7a79259698c0287b2ae0267cd2be2a90ddde32a0
David Ellison <davidehellison@gmail.com>
2019-01-12 08:58:24 -0800

    wrap tests in one test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,15 @@
 using DependencyTrees, Test
 
-include("test_tokens.jl")
-include("test_graphs.jl")
-include("test_slp3_ch13.jl")
-include("test_nivre08.jl")
-include("test_kubleretal09.jl")
-include("test_treebanks.jl")
-include("test_conllu.jl")
-include("test_dynamic.jl")
-include("test_arc_hybrid.jl")
-include("test_arc_swift.jl")
-include("test_feature_extractor.jl")
+@testset "DependencyTrees" begin 
+    include("test_tokens.jl")
+    include("test_graphs.jl")
+    include("test_slp3_ch13.jl")
+    include("test_nivre08.jl")
+    include("test_kubleretal09.jl")
+    include("test_treebanks.jl")
+    include("test_conllu.jl")
+    include("test_dynamic.jl")
+    include("test_arc_hybrid.jl")
+    include("test_arc_swift.jl")
+    include("test_feature_extractor.jl")
+end
