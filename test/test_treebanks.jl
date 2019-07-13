c69bb0a73c24bf74a6ab284e6c79ed1493b3f7be
David Ellison <davidehellison@gmail.com>
2019-07-13 11:38:32 -0700

    test: slight reorg for treebank reader tests

--- test/test_treebanks.jl
+++ test/test_treebanks.jl
@@ -2,35 +2,37 @@
 
     datadir = joinpath(@__DIR__, "data")
 
-    @testset "Typed" begin
-        T = TypedDependency
-        tb1 = TreebankReader{T}(joinpath(datadir, "wsj_0001.dp"), add_id=true)
-        tb2 = TreebankReader{T}(joinpath(datadir, "wsj_0001.ids.dp"), add_id=false)
-        @test deptype(tb1) == deptype(tb2) == T
-        @test collect(tb1) == collect(tb2)
-    end
+    @testset "Treebank Readers" begin
+        @testset "Typed" begin
+            T = TypedDependency
+            tb1 = TreebankReader{T}(joinpath(datadir, "wsj_0001.dp"), add_id=true)
+            tb2 = TreebankReader{T}(joinpath(datadir, "wsj_0001.ids.dp"), add_id=false)
+            @test deptype(tb1) == deptype(tb2) == T
+            @test collect(tb1) == collect(tb2)
+        end
 
-    @testset "Untyped" begin
-        T = UntypedDependency
-        tb1 = TreebankReader{T}(joinpath(datadir, "wsj_0001.untyped.dp"), add_id=true)
-        tb2 = TreebankReader{T}(joinpath(datadir, "wsj_0001.untyped.ids.dp"), add_id=false)
-        @test deptype(tb1) == deptype(tb2) == T
-        @test collect(tb1) == collect(tb2)
-    end
+        @testset "Untyped" begin
+            T = UntypedDependency
+            tb1 = TreebankReader{T}(joinpath(datadir, "wsj_0001.untyped.dp"), add_id=true)
+            tb2 = TreebankReader{T}(joinpath(datadir, "wsj_0001.untyped.ids.dp"), add_id=false)
+            @test deptype(tb1) == deptype(tb2) == T
+            @test collect(tb1) == collect(tb2)
+        end
 
-    @testset "CoNLLU Multiword Tokens" begin
-        tree = collect(TreebankReader{CoNLLU}(joinpath(datadir, "multiwordtoken.conllu")))[1]
-        @test length(tree) == 24
-        @test length(tree.mwts) == 1
-        @test length(tree.emptytokens) == 0
-    end
+        @testset "CoNLLU Multiword Tokens" begin
+            tree = collect(TreebankReader{CoNLLU}(joinpath(datadir, "multiwordtoken.conllu")))[1]
+            @test length(tree) == 24
+            @test length(tree.mwts) == 1
+            @test length(tree.emptytokens) == 0
+        end
 
 
-    @testset "CoNLLU Empty Tokens" begin
-        tree = collect(TreebankReader{CoNLLU}(joinpath(datadir, "emptytokens.conllu")))[1]
-        @test length(tree) == 6
-        @test length(tree.mwts) == 0
-        @test length(tree.emptytokens) == 1
+        @testset "CoNLLU Empty Tokens" begin
+            tree = collect(TreebankReader{CoNLLU}(joinpath(datadir, "emptytokens.conllu")))[1]
+            @test length(tree) == 6
+            @test length(tree.mwts) == 0
+            @test length(tree.emptytokens) == 1
+        end
     end
 
     @testset "Treebanks" begin
