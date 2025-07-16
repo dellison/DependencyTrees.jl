21559ef4b0806351771ddb0ec5fa931fa7a69929
David Ellison <david@davidellison.org>
2025-07-15 23:55:27 -0700

    tarjan's algorithm (i.e., cycle detection for graphs) seems to work

--- src/graph_parsing/graphs.jl
+++ src/graph_parsing/graphs.jl
@@ -37,19 +37,39 @@ end
 
 # cycle detection
 
+function find_cycles(graph::Vector{Int})
+    sccs = tarjan(graph)
+    return filter(scc -> length(scc) > 1, sccs)
+end
+
+has_cycles(graph::Vector{Int}) =
+    any(scc -> length(scc) > 1, tarjan(graph))
+
+
+"""
+    tarjan(tree)
+
+Find strongly-connected components using Tarjan's algorithm.
+"""
+function tarjan end
+
+tarjan(G::DependencyTree) = tarjan(G.arcs)
+
 function tarjan(graph::Vector{Int})
     n = length(graph)
+    # edges[i] is the set of the dependent tokens
     edges = [Set{Int}() for _=1:n]
     for (i, h) in enumerate(graph)
-        push!(edges[i], h)
+        iszero(h) || push!(edges[h], i)
     end
-    indices  = -ones(Int, n)
+    indices = -ones(Int, n)
     lowlinks = -ones(Int, n)
-    onstack  = falses(n)
-    stack    = Int[]
-    index    = 0
-    sccs     = Set{Int}[]
-    function strongconnect(v, index, stack)
+    index = 1
+    onstack = falses(n)
+    stack = Int[]
+    sccs = Set{Int}[]
+    function strongconnect(v)
+        # set the depth index for v to the smallest unused index
         indices[v] = index
         lowlinks[v] = index
         index += 1
@@ -57,29 +77,83 @@ function tarjan(graph::Vector{Int})
         onstack[v] = true
         for w in edges[v]
             if indices[w] < 0
-                strongconnect(w, index, stack)
+                # Successor w has not yet been visited; recurse on it
+                strongconnect(w)
                 lowlinks[v] = min(lowlinks[v], lowlinks[w])
             elseif onstack[w]
+                # w is in the stack, therefore in the current SCC.  If
+                # w is not on stack, then (v, w) is an edge pointing
+                # to an SCC already found and must be ignored
                 lowlinks[v] = min(lowlinks[v], indices[w])
             end
         end
+        # if v is a root node, pop the stack and generate an SCC
         if lowlinks[v] == indices[v]
             scc = Set{Int}()
-            while stack[end] != v
+            while true
                 w = pop!(stack)
                 onstack[w] = false
                 push!(scc, w)
+                w == v && break
             end
-            w = pop!(stack)
-            onstack[w] = false
-            push!(scc, w)
             push!(sccs, scc)
         end
     end
     for v in 1:n
         if indices[v] < 0
-            strongconnect(v, index, stack)
+            strongconnect(v)
         end
     end
-    return sccs
+    return sccs    
 end
+
+
+# decoding
+
+struct CLENode{T}
+    index::T
+    incoming::BitVector
+end
+
+function CLENode(scores::AbstractMatrix, index)
+    incoming = trues(size(scores, 1))
+    for i in index
+        incoming[i] = false
+    end
+    return CLENode(index, incoming)
+end
+
+function combine(nodes...)
+    index = cat(node.index for node in nodes)
+    incoming = (|).(node.incoming for node in nodes)
+    return CLENode(index, incoming)
+end
+
+
+"""
+    chu_liu_edmonds(G)
+
+todo
+"""
+function chu_liu_edmonds end
+
+chu_liu_edmonds(G::DependencyGraph) = chu_liu_edmonds(G.arcs)
+
+function chu_liu_edmonds(G::AbstractMatrix)
+    nodes = [CLENode(G, i) for i in 1:size(G, 1)]
+    return _chu_liu_edmonds(G, nodes)
+end
+
+function _chu_liu_edmonds(G, nodes)
+    prediction = greedy_predict(G, nodes)
+    cycles = find_cycles(prediction)
+    if !isempty(cycles)
+        cycle = pop!(cycles)
+        # recurse (todo lol)
+    else
+        return prediction
+    end
+end
+
+greedy_predict(G, nodes) =
+    [argmax(G[node.incoming, node.index]) for node in nodes]
--- test/test_graphparsing.jl
+++ test/test_graphparsing.jl
@@ -4,11 +4,21 @@
 
     @testset "Cycle Detection" begin
         @testset "Tarjan's Algorithm" begin
-            using DependencyTrees.GraphParsing: tarjan
+            using DependencyTrees.GraphParsing: tarjan, find_cycles, has_cycles
 
             @testset "One Cycle" begin
-                @test Set([1, 2]) in tarjan([2, 1])
-                @test Set([1, 2, 3]) in tarjan([2, 3, 1])
+                @testset "1 2" begin
+                    sccs = tarjan([2, 1])
+                    cycles = find_cycles([2, 1])
+                    @test sccs == cycles
+                    @test Set([1, 2]) in cycles
+                    @test length(cycles) == 1
+                end
+                @testset "1 2 3" begin
+                    cycles = tarjan([2, 3, 1])
+                    @test Set([1, 2, 3]) in cycles
+                    @test length(cycles) == 1
+                end
             end
 
             @testset "Two Cycles" begin
@@ -17,21 +27,54 @@
                 @test Set([3, 4]) in cycles
                 @test length(cycles) == 2
             end
+
+            @testset "'Eat!'" begin
+                # e.g., a sentence like "stop!" or "eat!"
+                sccs = tarjan([0])
+                cycles = find_cycles([0])
+                @test length(sccs) == 1
+                @test length(cycles) == 0
+            end
+
+            @testset "'I ate.'" begin
+                tree = [2, 0]
+                sccs = tarjan(tree)
+                cycles = find_cycles(tree)
+                # @show tree cycles
+                @test length(sccs) == length(tree)
+                @test length(cycles) == 0
+            end
+
+            @testset "'I ate fish'" begin
+                tree = [2, 0, 2]
+                sccs = tarjan(tree)
+                cycles = find_cycles(tree)
+                @test length(sccs) == length(tree)
+                @test length(cycles) == 0
+            end
+
+            @testset "I ate fish with ketchup" begin
+                tree = [2, 0, 2, 3, 4, 2]
+                sccs = tarjan(tree)
+                cycles = find_cycles(tree)
+                @test length(sccs) == length(tree)
+                @test length(cycles) == 0
+            end
         end
     end
 
     @testset "Chu-Liu/Edmonds" begin
-        
         graph = DependencyGraph(rand(10, 10))
     end
 
     @testset "'Book that flight'" begin
+        using DependencyTrees.GraphParsing: chu_liu_edmonds
 
         scores = reshape([12, 6, 5, 5, 4, 7, 7, 8, 4], (3, 3))
         graph = DependencyGraph(scores)
         @test getarc(graph, 0, 1) == 12
         # @test getarc(graph, 0, :) == [12, 4, 4]
-        
+        # @show chu_liu_edmonds(graph)
     end
 
 end
